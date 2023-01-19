239ee9fb0717c63a75c19f49c949b01493538882
Shawn McGee <s.mcgeek07@gmail.com>
2023-01-19 01:16:40 -0700

    Working on post template!

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,9 @@ module.exports = function(eleventyConfig) {
   // eleventyConfig.addPassthroughCopy("./src/**/*.css")
   eleventyConfig.addPlugin(eleventyNavigation     )
   eleventyConfig.addPlugin(eleventySyntaxHighlight)
+  eleventyConfig.addFilter("snippet", (value) => {
+    return value.length > 144 ? value.substring(0, 144) + "..." : value
+  })
   return { dir: { 
     input : './src/', 
     output: './dist/'
--- src/posts/another_test.md
+++
@@ -1,7 +0,0 @@
----
-title: Another Test!
-layout: layouts/post
-date: "2023-01-18"
----
-
-This is also a test post!
