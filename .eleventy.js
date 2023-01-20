5448149685d8439f774a26c3bf5c14dc601a8674
Shawn McGee <s.mcgeek07@gmail.com>
2023-01-20 08:45:15 -0700

    Removed JSDOM

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,5 @@
 const eleventyNavigation      = require("@11ty/eleventy-navigation")
 const eleventySyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight")
-const { JSDOM } = require('jsdom')
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/asset/**/*" )
@@ -8,21 +7,15 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigation     )
   eleventyConfig.addPlugin(eleventySyntaxHighlight)
   eleventyConfig.addFilter("article", (value) => {
-    const DOM = new JSDOM(value)
-    const article =
-      DOM.window.document.querySelector("article")?.innerHTML ||
-      DOM.window.document.body.innerHTML ||
-      ''
     return `
     <head>
       <link href="/asset/css/style.css" rel="stylesheet" />
       <script src="https://unpkg.com/phosphor-icons"></script>
-
       <title>{{ title }}</title>
     </head>
-    <div class="prose">
-      ${ article }
-    </div>`
+    <article class="prose">
+      ${ value }
+    </article>`
   })
   return { dir: { 
     input : './src/', 
