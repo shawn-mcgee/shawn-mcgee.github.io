dddee2d5a3b1bbe2927bfd89f009f76b87a4b1b5
Shawn McGee <s.mcgeek07@gmail.com>
2023-01-26 01:07:42 -0700

    Fixed landing page on mobile

--- .eleventy.js
+++ .eleventy.js
@@ -2,14 +2,14 @@ const eleventyNavigation      = require("@11ty/eleventy-navigation")
 const eleventySyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight")
 
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("./src/asset/**/*" )
+  eleventyConfig.addPassthroughCopy("./src/assets/**/*" )
   // eleventyConfig.addPassthroughCopy("./src/**/*.css")
   eleventyConfig.addPlugin(eleventyNavigation     )
   eleventyConfig.addPlugin(eleventySyntaxHighlight)
   eleventyConfig.addFilter("article", (value) => {
     return `
     <head>
-      <link href="/asset/css/style.css" rel="stylesheet" />
+      <link href="/assets/css/style.css" rel="stylesheet" />
       <script src="https://unpkg.com/phosphor-icons"></script>
       <title>{{ title }}</title>
     </head>
