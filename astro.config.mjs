439cafcf32b3816f3acf93326262600ba7cd096f
shawn-mcgee <s.mcgeek07@gmail.com>
2025-03-16 12:13:52 -0600

    Add mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import svelte from '@astrojs/svelte';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [svelte()]
+  integrations: [svelte(), mdx()]
 });
